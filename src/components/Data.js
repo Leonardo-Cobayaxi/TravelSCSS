7fc4b6551dcfa1f545ac0c6644d4d8581a675151
leonardo <leonardokoba80@gmail.com>
2023-05-19 15:44:17 -0300

    new places

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,6 +2,11 @@ import paris from './assets/paris.jpg';
 import barcelona from './assets/barcelona.jpg'
 import moscow from './assets/moscow.jpg';
 import newyork from './assets/newyork.jpg';
+import yarham from './assets/yharnam-.jpg';
+import pandora from './assets/pandora-large.jpg';
+import dunwall from './assets/dunwall-dark.jpg';
+import columbia from './assets/columbia-dark.jpg';
+
 
 export const Data = [
     {
@@ -11,9 +16,21 @@ export const Data = [
         className: 'data-items'
     },
     {
+        image: yarham,
+        text: 'Healing Church ,  Yharnam ',
+        status: 'Enjoy the rich and isolationist city Just Clicking one Button',
+        className: 'data-items'
+    },
+    {
         image: barcelona,
         text: 'Barcelona ,  Spain',
         status: 'Enjoy Our one of the Biggest Tour in Barcelona Just Clicking one Button',
+        className: 'data-items'
+    },
+    {
+        image: columbia,
+        text: 'Columbia ,  USA',
+        status: 'Enjoy the Floating city of Columbia Just Clicking one Button',
         className: 'data-items'
     },
     {
@@ -21,11 +38,25 @@ export const Data = [
         text: 'Moscow ,  Russia',
         status: 'Enjoy Our one of the Biggest Tour in Moscow Just Clicking one Button',
         className: 'data-items'
-    },
+    }
+    ,
     {
         image: newyork,
         text: 'New York ,  USA',
         status: 'Enjoy Our one of the Biggest Tour in New York Just Clicking one Button',
         className: 'data-items'
+    },
+    {
+        image: pandora,
+        text: 'Wasteland ,  Pandora',
+        status: 'Enjoy the peculiar wildlife and people Just Clicking one Button',
+        className: 'data-items'
+    },
+    {
+        image: dunwall,
+        text: 'Dunwall ,  Gristol',
+        status: "Enjoy the grim place, polluted, and heavily stratified Just Clicking one Button",
+        className: 'data-items'
     }
+
 ]
--- src/components/Second.js
+++ src/components/Second.js
@@ -4,6 +4,11 @@ import small from './assets/small-paris.jpg'
 import barca from './assets/barcelona.jpg'
 import moscow from './assets/moscow.jpg'
 import newyork from './assets/newyork.jpg'
+import yarham from './assets/yharnam-small.jpg';
+import pandora from './assets/pandora-small.jpg';
+import dunwall from './assets/dunwall-alley.jpg';
+import columbia from './assets/columbia-light.jpg';
+
 const Second = () => {
     return (
         <section className="second">
@@ -14,16 +19,32 @@ const Second = () => {
                     <p>Paris, France 2011</p>
                 </div>
                 <div>
-                    <img src={barca} alt="paris" />
+                    <img src={yarham} alt="yharnam" />
+                    <p>Healing Church , Yharnam 1896</p>
+                </div>
+                <div>
+                    <img src={barca} alt="barcelona" />
                     <p>Barcelona, Spain 2019</p>
                 </div>
                 <div>
-                    <img src={moscow} alt="paris" />
+                    <img src={columbia} alt="columbia" />
+                    <p>Columbia , USA 1912</p>
+                </div>
+                <div>
+                    <img src={moscow} alt="moscow" />
                     <p>Moscow, Russia 2017</p>
                 </div>
                 <div>
-                    <img src={newyork} alt="paris" className='new-york' />
+                    <img src={dunwall} alt="dunwall" />
+                    <p>Dunwall , Gristol 1837</p>
+                </div>
+                <div>
+                    <img src={newyork} alt="new york" />
                     <p>NYC, USA 2020</p>
+                </div>
+                <div>
+                    <img src={pandora} alt="pandora" />
+                    <p>Wasteland , Pandora 2864</p>
                 </div>
             </div>
 
